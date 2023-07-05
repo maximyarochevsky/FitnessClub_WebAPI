c36b0a010842da417edd24f24895c96eb0fbe4bb
maximyarochevsky <maximyarochevskygithub@gmail.com>
2023-07-05 11:29:24 +0300

    Сущность тренера

---
+++ Core/FitnessClub.Domain/Entities/CoachEntity/Coach.cs
@@ -0,0 +1,17 @@
+﻿using FitnessClub.Domain.Entities.Base;
+using FitnessClub.Domain.Entities.ClientEntity;
+
+namespace FitnessClub.Domain.Entities.CoachEntity;
+
+public class Coach : Entity
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string? Patronymic { get; set; }
+    public int Experience { get; set; }
+    public decimal Price { get; set; }
+    public int Age { get; set; }
+    public string Description { get; set; }
+    public ICollection<Client>? Clients { get; set; }
+}
+
