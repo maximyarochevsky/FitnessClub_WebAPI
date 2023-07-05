1dc6d8f60e8d22e49fecbe4d0a3f59cdaca85ade
maximyarochevsky <maximyarochevskygithub@gmail.com>
2023-07-05 10:24:56 +0300

    Правки в папке с контекстом БД, создание контекста БД

--- Infrastructure/FitnessClub.Persistence/EntityTypeConfigurations/FitnessClubConfiguration.cs
+++ Infrastructure/FitnessClub.Persistence/EntityTypeConfigurations/FitnessClubConfiguration.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace FitnessClub.Persistence.EntityTypeConfigurations
 {
-    public class FitnessClubConfiguration: IEntityTypeConfigurations
+    public class FitnessClubConfiguration: IEntityTypeConfiguration<>
 
     {
     }
