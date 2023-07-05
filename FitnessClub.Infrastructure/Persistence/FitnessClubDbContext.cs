0ef9548a8279669188f6cc0169054e11d7f4ff71
maximyarochevsky <maximyarochevskygithub@gmail.com>
2023-07-05 11:36:55 +0300

    Добавление таблиц в контекст БД
    Добавление таблиц в контекст БД

--- FitnessClub.Infrastructure/Persistence/FitnessClubDbContext.cs
+++ FitnessClub.Infrastructure/Persistence/FitnessClubDbContext.cs
@@ -1,10 +1,22 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FitnessClub.Domain.Entities.ClientEntity;
+using FitnessClub.Domain.Entities.CoachEntity;
+using FitnessClub.Domain.Entities.SubscriptionEntity;
+using Microsoft.EntityFrameworkCore;
 
 namespace FitnessClub.Infrastructure.Persistence;
 
 public class FitnessClubDbContext: DbContext
 {
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Coach> Coachs { get; set; }
+    public DbSet<Subscription> Supscriptions { get; set; }
 
+    public FitnessClubDbContext(DbContextOptions<FitnessClubDbContext> options)
+        : base(options) { }
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
 }
 
