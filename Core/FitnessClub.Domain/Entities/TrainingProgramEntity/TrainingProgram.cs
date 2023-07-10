b9cf75c64d26edf357c0170b0cca10f2903850dc
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-07-10 14:24:13 +0300

    Сущность программы тренировок

---
+++ Core/FitnessClub.Domain/Entities/TrainingProgramEntity/TrainingProgram.cs
@@ -0,0 +1,12 @@
+﻿using FitnessClub.Domain.Entities.Base;
+
+namespace FitnessClub.Domain.Entities.TrainingProgramEntity;
+public class TrainingProgram : Entity
+{
+    public string Name { get; set; }
+    public double? Rating { get; set; }
+    public string Plan { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+}
+
